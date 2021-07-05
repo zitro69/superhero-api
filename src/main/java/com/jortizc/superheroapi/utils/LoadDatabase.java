1e0971ab7ae2ef9c050cf02e5e28ba3e47c9acbc
Javier Ortiz <jortizc96@gmail.com>
2021-07-05 13:42:08 +0200

    [LoadDatabase.java]: Added file

---
+++ src/main/java/com/jortizc/superheroapi/utils/LoadDatabase.java
@@ -0,0 +1,26 @@
+package com.jortizc.superheroapi.utils;
+
+import com.jortizc.superheroapi.model.Superhero;
+import com.jortizc.superheroapi.model.repository.SuperheroRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    public CommandLineRunner initDatabase (SuperheroRepository repository) {
+        return args -> {
+            LOG.info("Database preloading data: " + repository.save(new Superhero("Superman")).getName());
+            LOG.info("Database preloading data: " + repository.save(new Superhero("Spider-man")).getName());
+            LOG.info("Database preloading data: " + repository.save(new Superhero("Lobezno")).getName());
+            LOG.info("Database preloading data: " + repository.save(new Superhero("Manolito")).getName());
+            LOG.info("Database preloading data: " + repository.save(new Superhero("Super Lópe")).getName());
+        };
+    }
+}
