60daade28cfcfb43edc98231034b37704af7dbe9
Javier Ortiz <jortizc96@gmail.com>
2021-07-05 12:06:05 +0200

    [SuperheroController.java]: Done: getAllSuperhero, postNewSuperhero, getSuperheroById.

--- src/main/java/com/jortizc/superheroapi/controller/SuperheroController.java
+++ src/main/java/com/jortizc/superheroapi/controller/SuperheroController.java
@@ -1,4 +1,53 @@
 package com.jortizc.superheroapi.controller;
 
+import com.jortizc.superheroapi.model.Superhero;
+import com.jortizc.superheroapi.model.repository.SuperheroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class SuperheroController {
+    private final SuperheroRepository repository;
+
+    public SuperheroController(SuperheroRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/superhero")
+    public List<Superhero> getAllSuperhero() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/superhero")
+    public Superhero postNewSuperhero(@RequestBody Superhero newSuperhero) {
+        return repository.save(newSuperhero);
+    }
+
+    @GetMapping("/superhero/search")
+    public List<Superhero> getSuperherosByName (@RequestParam String name) {
+        return null;
+    }
+
+    @GetMapping("/superhero/{id}")
+    public Optional getSuperheroById(@PathVariable Long id) {
+        return repository.findById(id);
+    }
+
+    @PutMapping("/superhero/{id}")
+    public Optional updateSuperhero(@RequestBody Superhero updatedSuperhero, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(superhero -> {
+                    superhero.setName(updatedSuperhero.getName());
+                    return repository.save(superhero);
+                });
+    }
+
+    @DeleteMapping ("/superhero/{id}")
+    public void deleteSuperhero (@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 }
