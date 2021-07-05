0f57b5d5181c15017d6e5d5d879fbec1dc7f4ecf
Javier Ortiz <jortizc96@gmail.com>
2021-07-05 11:52:07 +0200

    [Superhero.java]: Create superhero POJO.

--- src/main/java/com/jortizc/superheroapi/model/Superhero.java
+++ src/main/java/com/jortizc/superheroapi/model/Superhero.java
@@ -1,4 +1,35 @@
 package com.jortizc.superheroapi.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "superhero")
 public class Superhero {
+    private @Id @GeneratedValue Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Superhero() {
+    }
+
+    public Superhero(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
